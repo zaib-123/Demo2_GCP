56e3a396bf3b35d7b9dd2439a104f37f2202836b
zaib-123 <65386967+zaib-123@users.noreply.github.com>
2024-02-22 19:28:27 +0000

    python update

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask 
 import os
+import sys
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def wish():
     message = "{name} is a leading service provider"
     return message.format(name=os.getenv("NAME", "Networklogic"))
+
 if __name__ == "__main__":
-   app. run (host='0.0.0.0', port=8080)
+    host = '0.0.0.0' if len(sys.argv) < 2 else sys.argv[1]
+    port = 8080 if len(sys.argv) < 3 else int(sys.argv[2])
+    app.run(host=host, port=port)
