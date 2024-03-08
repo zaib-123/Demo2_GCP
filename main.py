92fea331ceaffe75883bb44cd7f1e333d1627299
zaib khan <khanzaib368@gmail.com>
2024-03-08 16:12:18 +0000

    update 21

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def wish():
     message = "{name} is a leading service provider"
     return message.format(name=os.getenv("NAME", "Networklogic"))
 if __name__ == "__main__":
-   app. run (host='0.0.0.0', port=80)
+   app. run (host='0.0.0.0', port=8080)
